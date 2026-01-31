a5b69903def7eb069d32eb64c0d7990dc19eb1d0
1bgn <137element@gmail.com>
2026-02-01 01:45:02 +0800

    update

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+      },
+    },
+  },
+}
